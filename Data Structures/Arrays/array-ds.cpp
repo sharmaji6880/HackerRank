bfc3f8a7c6abf38b3e3f041cbb433ac227ff0c74
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-05-07 15:18:56 +0530

    Create array-ds.cpp

---
+++ Data Structures/Arrays/array-ds.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    int size;
+    cin>>size;
+    vector<int> arr(size);
+    for(int i=0;i<size;i++) {
+        cin>>arr[i];
+    }
+    vector<int> arrs(size);
+    int num=size-1;
+    
+    for(int i=num;i>=0;i--) {
+        arrs[num-i]=arr[i];
+    }
+    
+    for(int i=0;i<size;i++) {
+        if(i==size-1) {
+            cout<<arrs[i];
+            break;
+        }
+        cout<<arrs[i]<<" ";
+    }
+    return 0;
+}
