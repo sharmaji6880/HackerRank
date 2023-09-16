a2396dc32fbc16fbc891f912a15c449399df2967
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-09-16 16:02:35 +0530

    Create java-singleton-pattern.java

---
+++ Java/Advanced/java-singleton-pattern.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    public String str;
+    private Singleton() {
+        
+    }
+    static Singleton getSingleInstance() {
+        Singleton obj = new Singleton();
+        return obj;
+        
+    }
+    
+}
